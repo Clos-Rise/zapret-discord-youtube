01a7c82a96307ebf1be542e206b90efb764af9d3
Darkester <95033598+Clos-Rise@users.noreply.github.com>
2024-10-10 15:27:04 +0500

    Update discord.bat

--- discord.bat
+++ discord.bat
@@ -6,13 +6,37 @@ chcp 65001 >nul
 set scriptPath=%~dp0
 set "path_no_spaces=%scriptPath: =%"
 if not "%scriptPath%"=="%path_no_spaces%" (
-    echo Путь содержит пробелы. 
+    echo Путь содержит пробелы.
     echo Пожалуйста, переместите скрипт в директорию без пробелов.
     pause
     exit /b
 )
 
 set BIN=%~dp0bin\
+
+if not exist "%BIN%winws.exe" (
+    echo Файл %BIN%winws.exe не найден.
+    pause
+    exit /b
+)
+
+if not exist "%~dp0list-discord.txt" (
+    echo Файл %~dp0list-discord.txt не найден.
+    pause
+    exit /b
+)
+
+if not exist "%BIN%quic_initial_www_google_com.bin" (
+    echo Файл %BIN%quic_initial_www_google_com.bin не найден.
+    pause
+    exit /b
+)
+
+if not exist "%BIN%tls_clienthello_www_google_com.bin" (
+    echo Файл %BIN%tls_clienthello_www_google_com.bin не найден.
+    pause
+    exit /b
+)
 
 start "zapret: discord" /min "%BIN%winws.exe" ^
 --wf-tcp=443 --wf-udp=443,50000-65535 ^
